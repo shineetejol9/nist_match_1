71d872ef7cde392877b66da4873c0ae29718286c
shineetejol9 <shineetejolpatro@gmail.com>
2025-10-19 12:42:12 +0530

    Fix AboutSection import and component name

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ export default function App() {
             <>
               <Header />
               <HeroSection />
-              <Aboutsection />
+              <AboutSection />
               <ProjectsSection />
               <ContactSection />
             </>
