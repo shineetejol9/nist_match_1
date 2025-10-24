4db73b1977d308ce906e9f743be8383251118ccf
shineetejol9 <shineetejolpatro@gmail.com>
2025-10-24 23:41:21 +0530

    Setup backend for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(), // React plugin for Vite
+    tailwindcss(),
+    react()
+
   ],
   build: {
     // Optional: increase warning limit if you have large chunks
@@ -19,4 +22,4 @@ export default defineConfig({
       },
     },
   },
-});
+})
